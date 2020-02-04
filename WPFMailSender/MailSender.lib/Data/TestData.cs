638d3d7024e6552f7b653882b2acdefb94886688
Notliver <akurganskii@gmail.com>
2020-02-04 08:08:51 +0300

    Add Test Data

---
+++ WPFMailSender/MailSender.lib/Data/TestData.cs
@@ -0,0 +1,24 @@
+﻿using MailSender.lib.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.lib.Data
+{
+    public static class TestData
+    {
+        public static List<Server> Servers { get; } = new List<Server>
+        {
+            new Server {Name = "Yandex", Address = "smtp.yandex.ru", Port = 465, Login = "DefaultLogin", Password = "DefaultPassword"},
+            new Server {Name = "Mail.ru", Address = "smtp.mail.ru", Port = 465, Login = "DefaultLogin", Password = "DefaultPassword"},
+            new Server {Name = "Yandex", Address = "smtp.gmail.com", Port = 465, Login = "DefaultLogin", Password = "DefaultPassword"}
+        };
+
+        public static List<Sender> Senders { get; } = new List<Sender>
+        {
+            new Sender {Name = "Test1", Address = "Test1"},
+            new Sender {Name = "Test2", Address = "Test2"},
+            new Sender {Name = "Test3", Address = "Test3"}
+        };
+    }
+}
